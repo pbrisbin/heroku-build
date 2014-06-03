1acd2cc28a55971e7e817bcdfea90ce8e4a1e946
patrick brisbin <pbrisbin@gmail.com>
2014-06-02 20:27:33 -0400

    Remove Internal, add initial implementation
    - Build datatype and JSON instances

--- src/HerokuBuild/Internal.hs
+++
@@ -1,3 +0,0 @@
-module HerokuBuild.Internal
-    (
-    ) where
--- test/HerokuBuildSpec.hs
+++ test/HerokuBuildSpec.hs
@@ -1,5 +1,8 @@
 module HerokuBuildSpec (main, spec) where
 
+import HerokuBuild
+import qualified Data.Text as T
+
 import Test.Hspec
 
 main :: IO ()
@@ -7,6 +10,47 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "someFunction" $ do
-    it "should work fine" $ do
-      True `shouldBe` False
+    describe "encodeBuild" $ do
+        it "encodes correctly" $ do
+            let expected = T.concat
+                    [ "{"
+                    ,   "\"source_blob\":{"
+                    ,     "\"url\":\"https://example.com\","
+                    ,     "\"version\":\"abc123\""
+                    ,   "}"
+                    , "}"
+                    ]
+
+                actual = encodeBuild $ Build
+                    undefined undefined (SourceBlob "https://example.com" "abc123")
+
+            actual `shouldBe` expected
+
+    describe "decodeBuild" $ do
+        it "decodes correctly" $ do
+            let response = T.concat
+                    [ "{"
+                    , "  \"created_at\": \"2014-05-09T19:43:56+00:00\","
+                    , "  \"id\": \"22f637e2-05ab-4e71-bb8a-8ea65c88577b\","
+                    , "  \"source_blob\": {"
+                    , "    \"url\": \"https://example.com\","
+                    , "    \"version\": \"abc123\""
+                    , "    },"
+                    , "  \"slug\": {"
+                    , "    \"id\": null"
+                    , "    },"
+                    , "  \"status\": \"pending\","
+                    , "  \"updated_at\": \"2014-05-09T19:43:56+00:00\","
+                    , "  \"user\": {"
+                    , "    \"email\": \"andy@heroku.com\","
+                    , "    \"id\": \"xxxxxxxxxxxxxxxxxxx\""
+                    , "  }"
+                    , "}"
+                    ]
+
+                expected = Right $ Build
+                    "22f637e2-05ab-4e71-bb8a-8ea65c88577b"
+                    Pending
+                    (SourceBlob "https://example.com" "abc123")
+
+            decodeBuild response `shouldBe` expected
