aa894b2f7810f7c174715d6f6e8cec9a5d84e0ae
patrick brisbin <pbrisbin@gmail.com>
2014-06-04 09:41:26 -0400

    Remove leftover code

--- src/HerokuBuild.hs
+++ src/HerokuBuild.hs
@@ -1,14 +1,5 @@
 module HerokuBuild (module X) where
 
-import qualified Data.Text as T
-
 import HerokuBuild.API as X
 import HerokuBuild.JSON as X
 import HerokuBuild.Options as X
-
-main :: IO ()
-main = withOptions run
-
-run :: Options -> IO ()
-run (Options (Start url version)) = postHeroku "/builds" $ newBuild url version
-run (Options (Status b)) = print . status =<< getHeroku ("/builds/" `T.append` b)
